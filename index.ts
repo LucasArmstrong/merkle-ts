b9b7de8aee56f1fa50c349bcdb5ecf64e41def9b
Lucas Armstrong <17970245+LucasArmstrong@users.noreply.github.com>
2022-06-04 15:14:07 -0400

    Adding type and additional logging to the example process node script

--- index.ts
+++ index.ts
@@ -3,8 +3,9 @@ import { MerkleTree } from './src/merkle/MerkleTree';
 
 try {
     const hashListArray = fs.readFileSync('hashList.txt').toString().split("\n");
-    const merkleHashList = new MerkleTree(hashListArray);
-    console.log(`Merkle root for hashList.txt: ${merkleHashList.root}`);
+    const merkleTree: MerkleTree = new MerkleTree(hashListArray);
+    console.log(`MerkleTree processed ${hashListArray.length} lines of text from the hashList.txt`);
+    console.log(`Merkle root for hashList.txt: ${merkleTree.root}`);
   } catch (err) {
     console.error(err);
 }
