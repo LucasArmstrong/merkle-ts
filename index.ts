3884302261bb97f6ee7c1f80dcdd3d6aea01deeb
Lucas Armstrong <17970245+LucasArmstrong@users.noreply.github.com>
2022-07-05 21:46:34 -0400

    Updated example for processing files in a directory

--- index.ts
+++ index.ts
@@ -1,11 +1,34 @@
 const fs = require('fs');
 import { MerkleTree } from './src/merkle/MerkleTree';
 
+const directoryPath = './src/examples/hashFiles';
+
 try {
-    const hashListArray = fs.readFileSync('hashList.txt').toString().split("\n");
-    const merkleTree: MerkleTree = new MerkleTree(hashListArray);
-    console.log(`MerkleTree processed ${hashListArray.length} lines of text from the hashList.txt`);
-    console.log(`Merkle root for hashList.txt: ${merkleTree.root}`);
+  console.log(`\n\n------------------- Processing single file -------------------`);
+  processFile(`${directoryPath}/hashList.txt`);
+  console.log(`\n\n------------------- Processing directory -------------------`);
+  processDirectory(directoryPath);
   } catch (err) {
     console.error(err);
+}
+
+function processFile(filePath: string): string {
+  const hashListArray = fs.readFileSync(filePath).toString().split("\n");
+  const merkleTree: MerkleTree = new MerkleTree(hashListArray);
+  console.log(`\nMerkleTree processed ${hashListArray.length} lines of text from the ${filePath}`);
+  console.log(`Merkle root for hashList.txt: ${merkleTree.root}`);
+  return merkleTree.root;
+}
+
+function processDirectory(directoryPath: string) {
+  fs.readdir(directoryPath, function (err: Error, files: []) {
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    }
+    files.map(file => {
+      return processFile(`${directoryPath}/${file}`);
+    });
+    const merkleTree: MerkleTree = new MerkleTree(files);
+    console.log(`\nMerkle root for ${directoryPath}/: ${merkleTree.root}`);
+  });
 }
